629edcc05053ea82ed4022d03fd134f58462ae8e
pablomanez <pablomanez@hotmail.com>
2018-05-10 23:07:43 +0200

    Fase 2 css

---
+++ hornojs.js
@@ -0,0 +1,21 @@
+//BOTONES DEL HORNO
+resS = false;
+resI = false;
+grill = false;
+fan = false;
+
+function resS(){
+		
+}
+
+function resI(){
+	
+}
+
+function grill(){
+	
+}
+
+function fan(){
+	
+}
