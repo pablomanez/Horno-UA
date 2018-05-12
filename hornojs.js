3a517c75b12165026099fa72c8016b577e252645
pablomanez <pablomanez@hotmail.com>
2018-05-12 12:17:40 +0200

    Responsif hecho, ahora el JS

--- hornojs.js
+++ hornojs.js
@@ -1,8 +1,11 @@
+const TIME_MAX = 1440		//TIEMPO MAXIMO = 1 DIA
+
 //BOTONES DEL HORNO
 b_resS = false;
 b_resI = false;
 b_grill = false;
 b_fan = false;
+b_puerta = false;
 
 
 
@@ -45,54 +48,136 @@ function fan(){
 
 //TEMPERATURA
 function tempMenos(num){
-	let input = document.querySelector('#inputTemp');
 	let range = 0;
 
 	num? range = 5 : range = 1;
 
-	input.value -= range;
+	document.querySelector('#inputTemp').innerText -= range;
 
-	if(input.value < 0) input.value = 0;
+	if(document.querySelector('#inputTemp').innerText < 0) 
+		document.querySelector('#inputTemp').innerText = 0;
+
+	updateTemp();
 }
 
 function tempMas(num){
-	let input = document.querySelector('#inputTemp');
 	let range = 0;
 
 	num? range = 5 : range = 1;
 
-	input.value -= -(range);
+	document.querySelector('#inputTemp').innerText -= (-range);
 
-	if(input.value > 270) input.value = 270;	
+	if(document.querySelector('#inputTemp').innerText > 270) 
+		document.querySelector('#inputTemp').innerText = 270;	
+
+	updateTemp();
+}
+
+function updateTemp(){
+	let valor = document.querySelector('#inputTemp').innerText;
+	let progress = document.querySelector('#progressTemp');
+
+	valor_aux = valor/2.7;
+
+	progress.setAttribute('style','width: '+valor_aux+'%;');
+	progress.setAttribute('aria-valuenow',valor_aux);
+	progress.innerText = valor+'ºC';
 }
 
 //TIEMPO
 function timeMenos(num){
-	let input = document.querySelector('#inputTime');
+	let tiempo = document.querySelector('#inputTime');
+	let tiempo_aux = tiempo.innerText.split(':');
+
+	let hora = tiempo_aux[0];
+	let minutos = tiempo_aux[1];
+
 	let range = 0;
 
-	num? range = 5 : range = 1;
+	num? range = 10 : range = 5;
 
-	input.value -= range;
+	minutos -= range;
 
-	if(input.value < 0) input.value = 0;
+	if(minutos<0){
+		minutos += 60;
+		
+		hora -= 1;
+		if(hora<10)		hora = '0'+hora;
+	}
+	
+	if(minutos<10)	minutos = '0'+minutos;
+
+	let update = hora+':'+minutos;
+
+	tiempo.innerText = update;
+
+	updateTime();
 }
 
 function timeMas(num){
-	let input = document.querySelector('#inputTime');
+	let tiempo = document.querySelector('#inputTime');
+	let tiempo_aux = tiempo.innerText.split(':');
+
+	let hora = tiempo_aux[0];
+	let minutos = tiempo_aux[1];
+
 	let range = 0;
 
-	num? range = 5 : range = 1;
+	num? range = 10 : range = 5;
 
-	input.value -= -(range);
+	minutos -= -(range);
 
-	if(input.value > 270) input.value = 270;	
-}
+	if(minutos>=60){
+		minutos -= 60;
+		
+		hora -= (-1);
+		if(hora<10)		hora = '0'+hora;
+	}
+	
+	if(minutos<10)	minutos = '0'+minutos;
 
-function updateTemp(){
-	console.log("se ha cambiado la temperatura wey");
+	let update = hora+':'+minutos;
+
+	tiempo.innerText = update;
+
+	updateTime();
 }
 
 function updateTime(){
+	let tiempo = document.querySelector('#inputTime').innerText;
+	let tiempo_aux = tiempo.split(':');
+
+	let hora = tiempo_aux[0];
+	let minutos = tiempo_aux[1];
+	
+	let total = minutos-(-hora*60);
+	total /= 14.4;
+
+	/*
+	console.log(hora);
+	console.log(minutos);
+	console.log(total);
+	*/
+
+	let progress = document.querySelector('#progressTime');
+
+	progress.setAttribute('style','width: '+total+'%;');
+	progress.setAttribute('aria-valuenow',total);
+	//progress.innerText = ;
+}
+
+//PUERTA
+function indPuerta(estado){
+	console.log('Puerta abierta: '+estado);
+
+	b_puerta = estado;
+
+	let puerta = document.querySelector('#lock_puerta');
+	
+	b_puerta? 
+		puerta.setAttribute("class","fas fa-lock-open fa-5x text-warning py-2 pl-2") 
+		: 
+		puerta.setAttribute("class","fas fa-lock fa-5x text-warning py-2");
 
 }
+electro.puerta(indPuerta);
